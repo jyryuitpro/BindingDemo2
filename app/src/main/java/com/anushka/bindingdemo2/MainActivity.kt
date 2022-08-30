ee0edec1c2cab6581a30007f026ab2a320334360
jyr <jyr@wavers.co.kr>
2022-08-30 10:20:20 +0900

    Coding Challenge 1 && Coding Challenge 1 Answer

--- app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
+++ app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
@@ -5,26 +5,49 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
+//    private lateinit var button: Button
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.control_button)
-        button.setOnClickListener {
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        button = findViewById(R.id.control_button)
+//        button.setOnClickListener {
+//            startOrStopProgressBar()
+//        }
+        binding.controlButton.setOnClickListener {
             startOrStopProgressBar()
         }
     }
 
     private fun startOrStopProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        if (progressBar.visibility == View.GONE) {
-            progressBar.visibility = View.VISIBLE
-            button.text = "Stop"
-        } else {
-            progressBar.visibility = View.GONE
-            button.text = "Start"
+//        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+//        if (progressBar.visibility == View.GONE) {
+//            progressBar.visibility = View.VISIBLE
+//            button.text = "Stop"
+//        } else {
+//            progressBar.visibility = View.GONE
+//            button.text = "Start"
+//        }
+//        if (binding.progressBar.visibility == View.GONE) {
+//            binding.progressBar.visibility = View.VISIBLE
+//            binding.controlButton.text = "Stop"
+//        } else {
+//            binding.progressBar.visibility = View.GONE
+//            binding.controlButton.text = "Start"
+//        }
+        binding.apply {
+            if (progressBar.visibility == View.GONE) {
+                progressBar.visibility = View.VISIBLE
+                controlButton.text = "Stop"
+            } else {
+                progressBar.visibility = View.GONE
+                controlButton.text = "Start"
+            }
         }
     }
 }
